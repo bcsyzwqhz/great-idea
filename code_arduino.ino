27be4a2340b761439f194aeecff9cf69aa2af3d3
bcsyzwqhz <makerlai@163.com>
2024-04-06 11:57:27 +0800

    First commit

---
+++ code_arduino.ino
@@ -0,0 +1,39 @@
+
+#include <U8g2lib.h>
+#include <Wire.h>
+#include <Adafruit_NeoPixel.h>
+
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+Adafruit_NeoPixel rgb_display_17= Adafruit_NeoPixel(3,17,NEO_GRB + NEO_KHZ800);
+
+void setup(){
+  Serial.begin(9600);
+  u8g2.setI2CAddress(0x3C*2);
+  u8g2.begin();
+  u8g2.enableUTF8Print();
+
+  rgb_display_17.begin();
+}
+
+void loop(){
+  u8g2.setFont(u8g2_font_wqy12_t_gb2312);
+  u8g2.setFontPosTop();
+  Serial.println(analogRead(SOUND));
+  u8g2.firstPage();
+  do
+  {
+    if (analogRead(SOUND) > 414) {
+      u8g2.setCursor(0,20);
+      u8g2.print("讲话");
+      rgb_display_17.setPixelColor(0,(0xff0000));
+      rgb_display_17.setPixelColor(1,(0xff0000));
+      rgb_display_17.setPixelColor(2,(0xff0000));
+
+    }
+    rgb_display_17.show();
+    rgb_display_17.setPixelColor(0,(0x000000));
+    rgb_display_17.setPixelColor(1,(0x000000));
+    rgb_display_17.setPixelColor(2,(0x000000));
+  }while(u8g2.nextPage());
+
+}
